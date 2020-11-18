5cd882d18c2a95dc0e1eab1dfefaf632f1da40e5
subekti404dev <61002089+subekti404dev@users.noreply.github.com>
2020-11-18 09:26:40 +0700

    Update RNRnNetworkPrinterModule.java

--- android/src/main/java/com/subekti/RNNetworkPrinter/RNRnNetworkPrinterModule.java
+++ android/src/main/java/com/subekti/RNNetworkPrinter/RNRnNetworkPrinterModule.java
@@ -102,7 +102,7 @@ public class RNRnNetworkPrinterModule extends ReactContextBaseJavaModule {
               // CASH DRAWER
               boolean openCashDrawer = command.getBoolean("openCashDrawer");
               if (openCashDrawer == true) {
-                out.write(PrinterCommand.POS_Set_Cashbox(0, 25, 250));
+                out.write(PrinterCommand.POS_Set_Cashbox(0, 10, 10));
               }
             } else if (command.hasKey("feed")) {
               int feed = command.getInt("feed");
